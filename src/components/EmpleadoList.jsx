a1867fc9478d40f60ba46b48f81424e049b83026
José Eugenio Navarro Bovi <jose.navarrobovi@alu.frt.utn.edu.ar>
2024-02-05 04:32:17 -0300

    Se soluciono un warning

--- src/components/EmpleadoList.jsx
+++ src/components/EmpleadoList.jsx
@@ -7,9 +7,9 @@ function EmpleadoList({ empleados }) {
     <div className="container mt-5">
         <div className="row mb-3">
       {empleados.map((empleado) => (
-        <div className="col-sm-6 col-md-4 col-lg-6 mb-4 mx-5">
-        <EmpleadoRow key={empleado.id} {...empleado} />
-        </div>
+                  <div key={empleado.id} className="col-sm-6 col-md-4 col-lg-6 mb-4 mx-5">
+                  <EmpleadoRow {...empleado} />
+                </div>
       ))}
     </div>
     </div>
